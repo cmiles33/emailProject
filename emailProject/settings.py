21763efef93c811468860e6602cdc05be0534eb2
caleb <caleb.miles33@gmail.com>
2021-09-02 16:24:17 -0500

    Heroku Push

--- emailProject/settings.py
+++ emailProject/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_zbr66_0q6ne6zba9vndnuu-8tnp4j7ld^h04htf#9@plo)n(g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://cmiles-translation-project.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
